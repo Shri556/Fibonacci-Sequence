75e44607877444d8213e1ee9cb2b9db88054d18f
Shri556 <121234036+Shri556@users.noreply.github.com>
2023-07-12 17:49:26 +0530

    Create Fibonacci_Seq.py

---
+++ Fibonacci_Seq.py
@@ -0,0 +1,20 @@
+import sys
+sys.set_int_max_str_digits(0)
+while True:
+    x = input("Please Enter a Number: ")
+
+    if x.isnumeric():
+        ab = int(x)
+        break
+    print("Please Enter a Valid Number")
+
+###Loop for fibonacci sequence
+a = 0
+b = 1
+while True:
+    a = b
+    b = a + b
+    
+    if len(str(a)) == ab:
+        print(a)
+        break
