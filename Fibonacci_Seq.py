33894067604bccbbea2ff61fc7230035221b627d
Shri556 <121234036+Shri556@users.noreply.github.com>
2023-07-15 17:39:01 +0530

    Fibonacci_Seq.py

--- Fibonacci_Seq.py
+++ Fibonacci_Seq.py
@@ -1,11 +1,19 @@
+
+
+
+
 import sys
 sys.set_int_max_str_digits(0)
 while True:
     x = input("Please Enter a Number: ")
 
     if x.isnumeric():
-        ab = int(x)
-        break
+        x = int(x)
+        if x == 0:
+            print("Enter a number above zero!!")
+        if x > 0:
+            ab = int(x)
+            break
     print("Please Enter a Valid Number")
 
 ###Loop for fibonacci sequence
